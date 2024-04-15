9d69dde23cdbdc16c80f12d66f866ea094663263
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-04-15 23:34:41 +0200

    feat: partialUsingArguments task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -240,8 +240,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...initialArgs) {
+  return function handler(...additionalArgs) {
+    return fn(...initialArgs, ...additionalArgs);
+  };
 }
 
 /**
