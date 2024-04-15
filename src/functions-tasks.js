294d136bab3e012dfbc91682fc16aa6c1c3cc408
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-04-15 23:39:30 +0200

    feat: getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -263,8 +263,12 @@ function partialUsingArguments(fn, ...initialArgs) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom - 1;
+  return function generatorHandler() {
+    currentId += 1;
+    return currentId;
+  };
 }
 
 module.exports = {
